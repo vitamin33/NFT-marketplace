675a915ce7cc62efc3682f02a2179f6e338fd8b5
Vitalii Serbyn <vivitseser@gmail.com>
2022-06-18 00:47:00 +0300

    Polish nft items UI and view nft metadata

--- components/navbar/index.tsx
+++ components/navbar/index.tsx
@@ -1,4 +1,5 @@
-/* This example requires Tailwind CSS v2.0+ */
+/* eslint-disable @next/next/no-img-element */
+
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
@@ -10,12 +11,11 @@ const navigation = [
     { name: 'Calendar', href: '#', current: false },
 ]
 
-// @ts-ignore
-function classNames(...classes) {
+function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(' ')
 }
 
-export default function Example() {
+export default function Navbar() {
     return (
         <Disclosure as="nav" className="bg-gray-800">
             {({ open }) => (
@@ -23,7 +23,7 @@ export default function Example() {
                     <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                         <div className="relative flex items-center justify-between h-16">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                                {/* Mobile menu button*/}
+                                {/* Mobile menu butt*/}
                                 <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                                     <span className="sr-only">Open main menu</span>
                                     {open ? (
@@ -36,13 +36,8 @@ export default function Example() {
                             <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
                                 <div className="flex-shrink-0 flex items-center">
                                     <img
-                                        className="block lg:hidden h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                                        alt="Workflow"
-                                    />
-                                    <img
-                                        className="hidden lg:block h-8 w-auto"
-                                        src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg"
+                                        className="hidden lg:block h-10 w-auto"
+                                        src="/images/page_logo.png"
                                         alt="Workflow"
                                     />
                                 </div>
---
+++ components/nft/list/index.tsx
@@ -0,0 +1,22 @@
+import {FunctionComponent} from "react";
+import NftItem from "../item";
+import {NftMeta} from "../../../types/types";
+
+type NftListProps = {
+    nfts: NftMeta[]
+}
+
+const NftList: FunctionComponent<NftListProps> = ({nfts}) => {
+    return (
+        <div className="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
+            {
+                nfts.map(nft =>
+                        <div key={nft.image} className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+                            <NftItem item={nft}/>
+                        </div>
+                    )
+            }
+        </div>
+    );
+}
+export default NftList
