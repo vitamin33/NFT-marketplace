7b9589c3b3a8762557254f25942639473b86e0cd
Vitalii Serbyn <vivitseser@gmail.com>
2022-06-20 13:09:24 +0300

    Add onAccountChanged listener and account data refactoring

--- components/hooks/web3/useAccount.ts
+++ components/hooks/web3/useAccount.ts
@@ -1,19 +1,64 @@
 import useSwr from "swr";
 import {CryptoHookFactory} from "@_types/hooks";
+import {useEffect} from "react";
 
-type AccountHookFactory = CryptoHookFactory<string>
+type UseAccountResponse = {
+    connect: () => void
+}
+
+type AccountHookFactory = CryptoHookFactory<string, UseAccountResponse>
 
 export type UseAccountHook = ReturnType<AccountHookFactory>
 
 // deps -> provider, ethereum, contract (web3State)
-export const hookFactory: AccountHookFactory = ({provider}) => (params) => {
+export const hookFactory: AccountHookFactory = ({
+                                                    ethereum,
+                                                    provider
+}) => (params) => {
 
-    const swrRes = useSwr(
+    const { data, mutate, ...swr} = useSwr(
         provider ? "web3/useAccount" : null,
-        () => {
-            return "Test user"
+        async () => {
+            const accounts = await provider!.listAccounts();
+            const account = accounts[0];
+            if (!account) {
+                throw "Can't retrieve and account! Please login into web3 wallet.";
+            }
+            return accounts[0];
+        },
+        {
+            revalidateOnFocus: false
         }
     )
-    return swrRes;
+
+    useEffect(() => {
+        ethereum?.on("accountsChanged", handleAccountsChanged);
+        return () => {
+            ethereum?.removeListener("accountsChanged", handleAccountsChanged)
+        }
+    });
+    const handleAccountsChanged = (...args: unknown[]) => {
+        const accounts = args[0] as string[];
+        if (accounts.length === 0) {
+            console.error("Please connect to Web3 wallet.");
+        } else if ( accounts[0] !== data) {
+            mutate(accounts[0]);
+        }
+    }
+
+    const connect = async () => {
+        try {
+            ethereum?.request({method: "eth_requestAccounts"});
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    return {
+        ...swr,
+        data,
+        mutate,
+        connect
+    };
 }
 
--- components/ui/navbar/index.tsx
+++ components/ui/navbar/index.tsx
@@ -1,16 +1,16 @@
 /* eslint-disable @next/next/no-img-element */
 
-import { Fragment } from 'react'
-import { Disclosure, Menu, Transition } from '@headlessui/react'
-import { BellIcon, MenuIcon, XIcon } from '@heroicons/react/outline'
+import {Fragment} from 'react'
+import {Disclosure, Menu, Transition} from '@headlessui/react'
+import {BellIcon, MenuIcon, XIcon} from '@heroicons/react/outline'
 import Link from "next/link";
 import {ActiveLink} from "../index";
 import {useWeb3} from "@providers/web3";
 import {useAccount} from "@hooks/web3";
 
 const navigation = [
-    { name: 'Marketplace', href: '/', current: true },
-    { name: 'Create NFT', href: '/nft/create', current: false },
+    {name: 'Marketplace', href: '/', current: true},
+    {name: 'Create NFT', href: '/nft/create', current: false},
 ]
 
 function classNames(...classes: string[]) {
@@ -18,23 +18,23 @@ function classNames(...classes: string[]) {
 }
 
 export default function Navbar() {
-    const { account } = useAccount();
-    console.log(account.data);
+    const {account} = useAccount();
 
     return (
         <Disclosure as="nav" className="bg-gray-800">
-            {({ open }) => (
+            {({open}) => (
                 <>
                     <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
                         <div className="relative flex items-center justify-between h-16">
                             <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
                                 {/* Mobile menu butt*/}
-                                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                                <Disclosure.Button
+                                    className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                                     <span className="sr-only">Open main menu</span>
                                     {open ? (
-                                        <XIcon className="block h-6 w-6" aria-hidden="true" />
+                                        <XIcon className="block h-6 w-6" aria-hidden="true"/>
                                     ) : (
-                                        <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                                        <MenuIcon className="block h-6 w-6" aria-hidden="true"/>
                                     )}
                                 </Disclosure.Button>
                             </div>
@@ -66,51 +66,68 @@ export default function Navbar() {
                                     </div>
                                 </div>
                             </div>
-                            <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                            <div
+                                className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                                 <button
                                     type="button"
                                     className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                                 >
                                     <span className="sr-only">View notifications</span>
-                                    <BellIcon className="h-6 w-6" aria-hidden="true" />
+                                    <BellIcon className="h-6 w-6" aria-hidden="true"/>
                                 </button>
 
                                 {/* Profile dropdown */}
-                                <Menu as="div" className="ml-3 relative">
-                                    <div>
-                                        <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
-                                            <span className="sr-only">Open user menu</span>
-                                            <img
-                                                className="h-8 w-8 rounded-full"
-                                                src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                                                alt=""
-                                            />
-                                        </Menu.Button>
-                                    </div>
-                                    <Transition
-                                        as={Fragment}
-                                        enter="transition ease-out duration-100"
-                                        enterFrom="transform opacity-0 scale-95"
-                                        enterTo="transform opacity-100 scale-100"
-                                        leave="transition ease-in duration-75"
-                                        leaveFrom="transform opacity-100 scale-100"
-                                        leaveTo="transform opacity-0 scale-95"
+
+                                { false ?
+                                    <Menu as="div" className="ml-3 relative">
+                                        <div>
+                                            <Menu.Button
+                                                className="bg-gray-800 flex text-sm rounded-full focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white">
+                                                <span className="sr-only">Open user menu</span>
+                                                <img
+                                                    className="h-8 w-8 rounded-full"
+                                                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+                                                    alt=""
+                                                />
+                                            </Menu.Button>
+                                        </div>
+                                        <Transition
+                                            as={Fragment}
+                                            enter="transition ease-out duration-100"
+                                            enterFrom="transform opacity-0 scale-95"
+                                            enterTo="transform opacity-100 scale-100"
+                                            leave="transition ease-in duration-75"
+                                            leaveFrom="transform opacity-100 scale-100"
+                                            leaveTo="transform opacity-0 scale-95"
+                                        >
+                                            <Menu.Items
+                                                className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                                                <Menu.Item>
+                                                    {({active}) => (
+                                                        <Link href="/profile">
+                                                            <a
+                                                                className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
+                                                            >
+                                                                Your Profile
+                                                            </a>
+                                                        </Link>
+                                                    )}
+                                                </Menu.Item>
+                                            </Menu.Items>
+                                        </Transition>
+                                    </Menu>
+                                    :
+                                    <button
+                                        onClick={() => {
+                                            account.connect();
+                                        }}
+                                        type="button"
+                                        className="inline-flex items-center px-3 py-1.5 border border-transparent text-xs font-medium rounded-full shadow-sm text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                                     >
-                                        <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                                            <Menu.Item>
-                                                {({ active }) => (
-                                                    <Link href="/profile">
-                                                        <a
-                                                            className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                                                        >
-                                                            Your Profile
-                                                        </a>
-                                                    </Link>
-                                                )}
-                                            </Menu.Item>
-                                        </Menu.Items>
-                                    </Transition>
-                                </Menu>
+                                        Connect Wallet
+                                    </button>
+                                }
+
                             </div>
                         </div>
                     </div>
