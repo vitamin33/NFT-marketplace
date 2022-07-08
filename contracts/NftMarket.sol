a3d54fcc007684c6b8406f0a16ea2eb4986fea10
Vitalii Serbyn <vivitseser@gmail.com>
2022-07-08 17:48:45 +0300

    Add ability to mint token and count items in NftMarketplace.sol

--- contracts/NftMarket.sol
+++ contracts/NftMarket.sol
@@ -1,7 +1,24 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NftMarket is ERC721URIStorage {
+
+    using Counters for Counters.Counters;
+
+    Counters.Counter private _listedItems;
+    Counters.Counter private _tokenIds;
+
     constructor() ERC721("Creatures NFT", "CNFT") {}
+
+    function mintToken(string memory tokenURI) public payable returns(uint) {
+        _tokenIds.increment();
+        _listedItems.increment();
+        uint newTokenId = _tokenIds.current();
+
+        _safeMint(msg.sender, newTokenId);
+        _setTokenURI(newTokenId, tokenURI);
+        return newTokenId;
+    }
 }
