935e356005a3b7c381281e188c897b7af623192d
Vitalii Serbyn <vivitseser@gmail.com>
2022-07-10 12:42:15 +0300

    Add test for checking tokenURI

--- test/NftMarket.test.js
+++ test/NftMarket.test.js
@@ -22,7 +22,13 @@ contract("NftMarket", accounts => {
         it("owner of first token should be address[0]", async () => {
             const owner = await _contract.ownerOf(1);
 
-            assert.equal(owner, accounts[0], "Owner of token is not matching accounts[0")
+            assert.equal(owner, accounts[0], "Owner of token is not matching accounts[0]")
+        })
+
+        it("first token should point to the correct token URI", async () => {
+            const actualTokenURI = await _contract.tokenURI(1);
+
+            assert.equal(actualTokenURI, tokenURI, "Token URI is not correctly set")
         })
     })
 })
