16d8db33b125255685b4a8a4a81832d534ceb963
Vitalii Serbyn <vivitseser@gmail.com>
2022-07-26 15:08:01 +0300

    Add useOwnedNfts hook

--- components/hooks/web3/setupHooks.ts
+++ components/hooks/web3/setupHooks.ts
@@ -2,11 +2,13 @@ import {Web3Dependencies} from "@_types/hooks";
 import { hookFactory as createAccountHook, UseAccountHook} from "@hooks/web3/useAccount";
 import { hookFactory as createNetworkHook, UseNetworkHook} from "@hooks/web3/useNetwork";
 import { hookFactory as createListedNftsHook, UseListedNftsHook} from "@hooks/web3/useListedNfts";
+import { hookFactory as createOwnedNftsHook, UseOwnedNftsHook} from "@hooks/web3/useOwnedNfts";
 
 export type Web3Hooks = {
     useAccount: UseAccountHook;
     useNetwork: UseNetworkHook;
     useListedNfts: UseListedNftsHook;
+    useOwnedNfts: UseOwnedNftsHook;
 }
 
 export type SetupHooks = {
@@ -17,6 +19,7 @@ export const setupHooks: SetupHooks = (deps) => {
     return {
         useAccount: createAccountHook(deps),
         useNetwork: createNetworkHook(deps),
-        useListedNfts: createListedNftsHook(deps)
+        useListedNfts: createListedNftsHook(deps),
+        useOwnedNfts: createOwnedNftsHook(deps)
     }
 }
